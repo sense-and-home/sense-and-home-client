d106742ed3266ac38094c12ccb1529244d5fe5e3
Kreoniz <kreoniz@mail.ru>
2025-10-21 19:15:45 +0600

    feat: add toggle to application section

--- src/sections/ApplicationSection.tsx
+++ src/sections/ApplicationSection.tsx
@@ -1,6 +1,11 @@
 import ApplicationBackground from "@/assets/img/application-background.webp";
+import { useState } from "react";
+
+type TMode = "consultation" | "ready";
 
 export function ApplicationSection() {
+  const [mode, setMode] = useState<TMode>("consultation");
+
   return (
     <div className="bg-background px-8 py-8" id="application-section">
       <div
@@ -9,27 +14,29 @@ export function ApplicationSection() {
           backgroundImage: `url(${ApplicationBackground})`,
         }}
       >
-        <div className="text-xl">
-          <h3 className="text-4xl font-extrabold">
-            Свяжитесь с нами для старта работ или персональной консультации!
-          </h3>
+        <div className="flex h-full flex-col justify-between text-xl">
+          <div>
+            <h3 className="text-4xl font-extrabold">
+              Свяжитесь с нами для старта работ или персональной консультации!
+            </h3>
 
-          <div className="mb-4 text-5xl leading-none font-extrabold">,,</div>
+            <div className="mb-4 text-5xl leading-none font-extrabold">,,</div>
 
-          <p>
-            Sense&Home - проверенный партнер в инновациях и повышении конверии
-            продаж среди застройщиков.
-          </p>
+            <p>
+              Sense&Home — проверенный партнер в инновациях и повышении
+              конверсии продаж среди застройщиков.
+            </p>
 
-          <p>
-            Для Вашего удобства, оставить заявку Вы можете как и с запросом на
-            бесплатную консультацию, так и с возможностью обсуждения уже
-            конкретных деталей.
-          </p>
+            <p>
+              Для вашего удобства оставить заявку вы можете как с запросом на
+              бесплатную консультацию, так и с возможностью обсуждения уже
+              конкретных деталей.
+            </p>
+          </div>
 
           <button
             type="button"
-            className="rounded-primary mt-10 inline-block bg-black px-10 py-3 text-xl font-bold text-white hover:cursor-pointer"
+            className="rounded-primary mt-10 inline-block w-fit bg-black px-10 py-3 text-xl font-bold text-white hover:cursor-pointer"
           >
             Запросить звонок
           </button>
@@ -37,20 +44,22 @@ export function ApplicationSection() {
 
         <div>
           <form
-            className="rounded-secondary flex h-full flex-col gap-2 bg-white/10 p-6 text-xl shadow-lg backdrop-blur-lg"
+            className="rounded-secondary flex h-full flex-col gap-3 bg-white/10 p-6 text-xl backdrop-blur-2xl"
             action="#"
           >
-            <div className="rounded-primary bg-black/20 text-xl font-bold">
+            <div className="rounded-primary bg-black/20 p-[2px] text-xl font-bold">
               <button
                 type="button"
-                className="bg-accent/70 w-1/2 rounded-[inherit] px-10 py-3 hover:cursor-pointer"
+                className={`${mode == "consultation" ? "bg-accent/70" : ""} w-1/2 rounded-[inherit] px-10 py-3 transition-colors hover:cursor-pointer`}
+                onClick={() => setMode("consultation")}
               >
                 Мне нужна консультация
               </button>
 
               <button
                 type="button"
-                className="w-1/2 rounded-[inherit] px-10 py-3 hover:cursor-pointer"
+                className={`${mode == "ready" ? "bg-accent/70" : ""} w-1/2 rounded-[inherit] px-10 py-3 transition-colors hover:cursor-pointer`}
+                onClick={() => setMode("ready")}
               >
                 Я готов к 1 этапу
               </button>
@@ -74,9 +83,28 @@ export function ApplicationSection() {
               placeholder="----------@mail.ru"
             />
 
+            <label
+              htmlFor="customFileInput"
+              className={`${mode === "consultation" ? "invisible opacity-0" : "opacity-100"} bg-foreground rounded-primary inline-flex min-w-md items-center justify-between px-8 py-3 text-black/50 transition-opacity hover:cursor-pointer`}
+            >
+              <span>Прикрепите информацию о проекте</span>
+
+              <svg width="21" height="25" viewBox="0 0 23 27" fill="none">
+                <path
+                  d="M7.7002 5.6998L11.6002 1.7998M11.6002 1.7998L15.5002 5.6998M11.6002 1.7998V14.7998M5.10049 10.8998C3.88905 10.8998 3.28332 10.8998 2.80552 11.0977C2.16844 11.3616 1.66199 11.8681 1.39811 12.5051C1.2002 12.9829 1.2002 13.5884 1.2002 14.7998V21.0398C1.2002 22.4959 1.2002 23.2235 1.48358 23.7797C1.73285 24.2689 2.13031 24.6674 2.61953 24.9167C3.17516 25.1998 3.90289 25.1998 5.35618 25.1998H17.8448C19.2981 25.1998 20.0248 25.1998 20.5804 24.9167C21.0697 24.6674 21.4678 24.2689 21.7171 23.7797C22.0002 23.224 22.0002 22.4971 22.0002 21.0438V14.7998C22.0002 13.5884 22.0001 12.9829 21.8021 12.5051C21.5383 11.8681 21.0322 11.3616 20.3952 11.0977C19.9174 10.8998 19.3116 10.8998 18.1002 10.8998"
+                  stroke="black"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+            </label>
+
+            <input type="file" id="customFileInput" className="hidden" />
+
             <button
               type="submit"
-              className="rounded-primary mt-16 bg-black px-8 py-3 font-bold text-white hover:cursor-pointer"
+              className="rounded-primary bg-black px-8 py-3 font-bold text-white hover:cursor-pointer"
             >
               Оставить заявку
             </button>
