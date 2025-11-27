2c6657d40b855bd2feabe151fc17bca156e21f1d
Kreoniz <kreoniz@mail.ru>
2025-11-27 18:46:28 +0600

    build: add and configure vite-bundle-analyzer Analyzing and optimizing bundle later

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,13 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
+import { analyzer } from "vite-bundle-analyzer";
+
+const shouldAnalyze = process.env.ANALYZE_DEBUG === "true";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), ...(shouldAnalyze ? [analyzer()] : [])],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
